d9788aa6dfb9214a025314b9765ad55fb927282e
JerryHu <jerryhu@JerryHus-Mac-mini.local>
2017-08-19 19:54:09 -0400

    First Observable and Observer

--- main.ts
+++ main.ts
@@ -1,1 +1,25 @@
-alert("OK!!");
+import { Observable, Observer } from "rxjs";
+
+let numbers = [1, 5, 10];
+let source = Observable.from(numbers);
+
+// An Observer needs 3 methods: next(), error(), complete()
+class MyObserver implements Observer<number> {
+
+    next(value) {
+        console.log(`value: ${value}`);
+    }
+
+    error(e) {
+        console.log(`'error: ${e}`);
+    }
+
+    complete() {
+        console.log('complete');
+    }
+}
+
+source.subscribe(new MyObserver());
+
+
+
