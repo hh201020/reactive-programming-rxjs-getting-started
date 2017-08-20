bdca677a23718c033b00196416e2ecb903236a6e
JerryHu <jerryhu@JerryHus-Mac-mini.local>
2017-08-20 15:53:39 -0400

    onErrorResumeNext

--- main.ts
+++ main.ts
@@ -1,11 +1,14 @@
 import { Observable } from "rxjs";
 
-let source = Observable.merge(
+let source = Observable.onErrorResumeNext(
     Observable.of(1),
     Observable.from([2,3,4]),
     Observable.throw(new Error("Stop!")),
     Observable.of(5)
-);
+).catch( e => {
+    console.log(`caught: ${e}`);
+    return Observable.of(10);
+})
 
 source.subscribe(
     value => console.log(`value: ${value}`),
