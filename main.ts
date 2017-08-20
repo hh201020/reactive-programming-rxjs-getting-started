397272b36bae478f08012ef81399f10937bb6afc
JerryHu <jerryhu@JerryHus-Mac-mini.local>
2017-08-20 16:03:29 -0400

    handle error

--- main.ts
+++ main.ts
@@ -1,17 +1,20 @@
-import { Observable } from "rxjs";
+import {Observable} from "rxjs";
+import {load, loadWithFetch} from "./loader";
 
-let source = Observable.merge(
-    Observable.of(1),
-    Observable.from([2,3,4]),
-    Observable.throw(new Error("Stop!")),
-    Observable.of(5)
-).catch( e => {
-    console.log(`caught: ${e}`);
-    return Observable.of(10);
-})
+let output = document.getElementById("output");
+let button = document.getElementById("button");
 
-source.subscribe(
-    value => console.log(`value: ${value}`),
-    error => console.log(`error: ${error}`),
-    () => console.log('complete')
-)
+let click = Observable.fromEvent(button, "click");
+
+function renderMovies(movies) {
+    movies.forEach(m => {
+        let div = document.createElement("div");
+        div.innerText = m.title;
+        output.appendChild(div);
+    });
+}
+
+loadWithFetch("moviess.json")
+    .subscribe( renderMovies,
+                e => console.log(`error: ${e}`),
+                () => console.log("complete!"));
