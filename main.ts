dd7d74d320e162aac24d859966523962ed7f087a
JerryHu <jerryhu@JerryHus-Mac-mini.local>
2017-08-19 20:34:33 -0400

    only gives x, y output

--- main.ts
+++ main.ts
@@ -1,6 +1,12 @@
 import {Observable} from "rxjs";
 
-let source = Observable.fromEvent(document, 'mousemove');
+let source = Observable.fromEvent(document, 'mousemove')
+    .map((e : MouseEvent) => {
+        return {
+            x: e.clientX,
+            y: e.clientY
+        }
+    });
 
 source.subscribe(
     value => console.log(value),
